926100416952ac4faa2d62f63f1afd27d326f77a
boris <boris.shchegolev.ext@deutsche-boerse.de>
2017-11-15 16:24:31 +0100

    #3 Run on Win
     - Added a *.BAT

---
+++ bundles/minerhealth.bat
@@ -0,0 +1,20 @@
+
+REM ############################################################################
+
+REM Folder where Claymore generates logs
+SET logDir=Clay98
+
+REM Provide a unique name for each machine here.
+REM By default machine's hostname is used.
+SET machine=`hostname`
+
+REM Enable or disable active fan control according to temperature
+SET fanControl=1
+
+REM Enable or disable remote restart (from web GUI)
+SET remoteReboot=1
+
+REM ############################################################################
+
+REM Running
+java -jar minerhealth.jar $machine $logDir $fanControl $remoteReboot
