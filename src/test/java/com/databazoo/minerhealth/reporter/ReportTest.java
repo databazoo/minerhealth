bf40d92eded9d95ee7666a7cdd2522233b641d18
Boris <boris@databazoo.com>
2018-01-06 23:52:14 +0100

    Cleanup
     - test fix

--- src/test/java/com/databazoo/minerhealth/reporter/ReportTest.java
+++ src/test/java/com/databazoo/minerhealth/reporter/ReportTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 public class ReportTest {
 	@Test
 	public void send() throws Exception {
-		assertEquals("OK", Report.up("testClientID", "TEST_RIG", 5, 66, 94.3).send());
-		assertEquals("OK", Report.start("testClientID", "TEST_RIG").send());
+		assertEquals("OK", Report.up("c107de54-40ef-43a4-99e3-acb5828c18ad", "TEST_RIG", 5, 66, 94.3).send());
+		assertEquals("OK", Report.start("c107de54-40ef-43a4-99e3-acb5828c18ad", "TEST_RIG").send());
 	}
 
 }
